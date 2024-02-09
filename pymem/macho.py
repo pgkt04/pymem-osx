39c3a9b9f276aac73ad4d3f1958719f7d4f3d769
qtkite <qtkite@pm.me>
2024-02-09 19:06:08 +1100

    simplified macho format

--- pymem/macho.py
+++ pymem/macho.py
@@ -1,20 +1,26 @@
 from typing import List
 
 class Segment:
-  def __init__(self) -> None: pass
+  def __init__(self, name: str, address: int) -> None:
+    self.name = name
+    self.address = address
+  def get_name(self) -> str: return self.name
+  def get_address(self) -> int: return self.address
 
 class Section:
   def __init__(self, name: str, address: int, segments: List[Segment]) -> None:
     self.name = name
     self.address = address
     self.segments = segments
+  def get_name(self) -> str: return self.name
+  def get_address(self) -> int: return self.address
+  def get_segments(self) -> List[Segment]: return self.segments
 
 class Modules:
-  def __init__(self, name, address) -> None:
+  def __init__(self, name: str, address: int) -> None:
     self.name = name
     self.address = address
     self.sections = []
-
   def add_sections(self, sections) -> None: self.sections.extend(sections)
   def add_section(self, section) -> None: self.sections.append(section)
   def get_name(self) -> str: return self.name
--- pymem/pymem.py
+++ pymem/pymem.py
@@ -21,11 +21,11 @@ class PyMem:
   def read_float(self, address: int) -> float: pass
   def read_double(self, address: int) -> float: pass
   def read_bool(self, address: int) -> bool: pass
-  def read_bytes(self, address: int, size: int) -> bytes: pass
-  def write_buffer(self, address: int, buffer: bytes) -> None: pass
+  def write_bytes(self, address: int, buffer: bytes) -> None: pass
   def write_int32(self, address: int, value: int) -> None: pass
   def write_int64(self, address: int, value: int) -> None: pass
   def write_float(self, address: int, value: float) -> None: pass
   def write_double(self, address: int, value: float) -> None: pass
   def write_bool(self, address: int, value: bool) -> None: pass
+  def get_base_address(self) -> int: return process.get_base_address(self.task)
   def get_modules(self, extended: bool = False) -> list: pass
